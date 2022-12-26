61a97fbb77fb39e0ac258ae77da1b74c5b4715e7
domenSedlar <domen.sedlar@gmail.com>
2022-12-26 15:58:55 +0100

    Central panel buttons are now generated acording to the list of paths inside of the paths datafile.

--- sound_wave/src/file_maneger/mod.rs
+++ sound_wave/src/file_maneger/mod.rs
@@ -1,18 +1,30 @@
 use std::collections::HashMap;
-use egui::*;
-use crate::MainWindow;
 
+use egui::*;
+//use gstreamer::glib::OptionArg::String as OtherString;
+use crate::MainWindow;
 mod fm_backend;
 
 pub struct FileManager{
     paths: HashMap<String,String>,
-    current_location: String
+    path_names: Vec<String>,
+    buttons: HashMap<String,String>,
+    current_location: String,
 }
 
 impl FileManager{
     pub fn default() -> FileManager{
+        let paths = fm_backend::get_paths();
+        let mut path_names= Vec::new();
+
+        for (k, v) in &paths{
+            path_names.push(String::from(k));
+        }
+
         return FileManager{
-            paths: fm_backend::get_paths(),
+            paths,
+            path_names,
+            buttons: HashMap::new(),
             current_location: String::new()
         }
     }
@@ -20,29 +32,71 @@ impl FileManager{
 
 impl super::MainWindow for FileManager{
     fn get_window(&mut self, ui: &mut egui::Ui) {
-        ui.label(
-            "",
-        );
-        ui.add_space(4.0);
-
-        let text_style = TextStyle::Body;
-        let row_height = ui.text_style_height(&text_style);
-        let num_rows = 10_000;
-        ScrollArea::vertical().auto_shrink([false; 2]).show_rows(
-            ui,
-            row_height,
-            num_rows,
-            |ui, row_range| {
-                for row in row_range {
-                    let text = format!("This is row {}/{}", row + 1, num_rows);
-                    if ui.button(text).clicked(){
-                        self.current_location = String::from("meow");
-                        println!("{}", self.current_location);
-                    };
-                }
-            },
-        );
+        if self.current_location == String::new(){
+            let text_style = TextStyle::Body;
+            let row_height = ui.text_style_height(&text_style);
+            let num_rows = self.path_names.len();
+            ScrollArea::vertical().auto_shrink([false; 2]).show_rows(
+                ui,
+                row_height,
+                num_rows,
+                |ui, row_range| {
+                    for row in row_range {
+                        let text = match self.path_names.get(row){
+                            None => {String::new()}
+                            Some(s) => {String::from(s)}
+                        };
+                        if text == String::new() {break;}
+                        if ui.button(text).clicked(){
+                            self.current_location = String::from("meow");
+                            println!("{}", self.current_location);
+                        };
+                    }
+                },
+            );
+        }
+        else {
+            let text_style = TextStyle::Body;
+            let row_height = ui.text_style_height(&text_style);
+            let num_rows = 10_000;
+            ScrollArea::vertical().auto_shrink([false; 2]).show_rows(
+                ui,
+                row_height,
+                num_rows,
+                |ui, row_range| {
+                    for row in row_range {
+                        let text = format!("This is row {}/{}", row + 1, num_rows);
+                        if ui.button(text).clicked(){
+                            self.current_location = String::from("meow");
+                            println!("{}", self.current_location);
+                        };
+                    }
+                },
+            );
+        }
     }
+}
+
+fn ScrollAreaTemplate(ui: &mut egui::Ui) {
+    ui.label(
+        "",
+    );
+    ui.add_space(4.0);
+
+    let text_style = TextStyle::Body;
+    let row_height = ui.text_style_height(&text_style);
+    let num_rows = 10_000;
+    ScrollArea::vertical().auto_shrink([false; 2]).show_rows(
+        ui,
+        row_height,
+        num_rows,
+        |ui, row_range| {
+            for row in row_range {
+                let text = format!("This is row {}/{}", row + 1, num_rows);
+                ui.label(text);
+            }
+        },
+    );
 }
 
 pub fn lorem_ipsum(ui: &mut egui::Ui) {
